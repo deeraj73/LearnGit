fc213556df454c422e402be52eb6438752dfd609
deeraj73 <cmdeeraj73@gmail.com>
2024-05-30 10:55:31 +0530

    Command_file

---
+++ git_commands.js
@@ -0,0 +1,16 @@
+const fun = add =(x,y) =>{
+    return x+y
+}
+
+// git config --global user.name "username"
+// git config --global user.email "useremail"
+// git config --global --list -- to list the username and email configured
+
+
+
+
+// git branch "branchname"-- for creating new branch
+// git checkout "branchname" -- for switch to branch
+// changes
+// first commit changes
+//git push(to particular branch)
